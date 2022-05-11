f36cfc28f01cba890b7becc0afdc2b24adbe1f83
Jelly38214 <goolezheng@gmail.com>
2022-05-11 11:48:09 +0800

    feat: Add transaction usge to  guarantee multiple opertions success.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,11 +3,12 @@ import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
+import { Event } from '../events/entities/event.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 // forFeature here registers typeorm in this module
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
